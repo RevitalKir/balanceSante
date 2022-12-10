10ef7680dc5ec8509a48400d6befb78186890a17
RevitalKir <33843890+RevitalKir@users.noreply.github.com>
2022-12-10 13:54:17 -0500

    update code with new email and promo

--- js/main.js
+++ js/main.js
@@ -59,48 +59,4 @@ $('.no-js').removeClass('no-js');
     distance: '0px'
   }, 300);
 
-
-  
-  //contact form
-  
-
-
-
-    // when the form is submitted
-  $('#contactForm').on('submit', function (e) {
-
-    // if the validator does not prevent form submit
-    if (!e.isDefaultPrevented()) {
-        var url = "contact.php";
-
-        // POST values in the background the the script URL
-        $.ajax({
-            type: "POST",
-            url: url,
-            data: $(this).serialize(),
-            success: function (data)
-            {
-                // data = JSON object that contact.php returns
-
-                // we recieve the type of the message: success x danger and apply it to the 
-                var messageAlert = 'alert-' + data.type;
-                var messageText = data.message;
-
-                // let's compose Bootstrap alert box HTML
-                var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                console.log ('alert:' + messageAlert + ' ' + 'test: ' + messageText )
-                // If we have messageAlert and messageText
-                if (messageAlert && messageText) {
-                    // inject the alert to .messages div in our form
-                    $('#contactForm').find('.messages').html(alertBox);
-                    // empty the form
-                    $('#contactForm')[0].reset();
-                }
-            }
-        });
-        return false;
-    }
-});
-
-
 })(jQuery); // End of use strict
